b9d12c60e087836735de0ec6c02eeba20e15a797
Sujay Bhowmick <sujay.bhowmick@sentifi.com>
2018-04-30 13:09:02 +0700

    Removed Cloud Stream with Spring Integration.

--- src/main/java/com/sujay/notifications/NotificationsApplication.java
+++ src/main/java/com/sujay/notifications/NotificationsApplication.java
@@ -1,29 +1,53 @@
 package com.sujay.notifications;
 
 import java.io.File;
+import org.springframework.amqp.core.AnonymousQueue;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
+import org.springframework.integration.amqp.dsl.Amqp;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.channel.MessageChannels;
 import org.springframework.integration.file.dsl.Files;
 import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.SubscribableChannel;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+
 
 @SpringBootApplication
 @RestController
 public class NotificationsApplication {
 
 	@Bean
+	SubscribableChannel notificationChannel(){
+		return MessageChannels.publishSubscribe().get();
+	}
+
+	@Bean
 	SubscribableChannel filesChannel(){
 		return MessageChannels.publishSubscribe().get();
+	}
+
+	@Bean
+	public Queue queue() {
+		return new Queue("notifications_queue");
+	}
+
+	@Bean
+	IntegrationFlow integrationFlowRabbit(ConnectionFactory connectionFactory){
+		return IntegrationFlows.from(Amqp.inboundGateway(connectionFactory, queue()))
+				.channel(notificationChannel())
+				.get();
 	}
 
 	@Bean
@@ -35,7 +59,18 @@ public class NotificationsApplication {
 				.get();
 	}
 
-	@GetMapping(value = "/files/{name}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	@CrossOrigin(origins = {"https://fiddle.jshell.net", "https://codepen.io"})
+	@GetMapping(value = "/notifications/{name}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	Flux<String> notifications(@PathVariable String name){
+		return Flux.create(sink -> {
+			MessageHandler handler = msg -> sink.next(new String(byte[].class.cast(msg.getPayload())));
+			sink.onCancel(() -> notificationChannel().unsubscribe(handler));
+			notificationChannel().subscribe(handler);
+		});
+	}
+
+
+	@GetMapping(value = "files/{name}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 	Flux<String> files(@PathVariable String name){
 		return Flux.create(sink -> {
 			MessageHandler handler = msg -> sink.next(String.class.cast(msg.getPayload()));
